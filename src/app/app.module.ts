b023befe8aa88bf1cedb4c50b8397e1ba2dfef95
Mina Faysal <m.faysal@glamera.net>
2024-03-01 01:59:07 +0200

    get date for home books cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BookDetailsComponent } from './pages/book-details/book-details.component';
 import { AuthorDetailsComponent } from './pages/author-details/author-details.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     BookDetailsComponent,
-    AuthorDetailsComponent
+    AuthorDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
