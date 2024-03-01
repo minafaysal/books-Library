5612f8849892b978c0cc900ac2d08d0d98df4c61
Mina Faysal <m.faysal@glamera.net>
2024-03-01 16:08:38 +0200

    update books data with covers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BookDetailsComponent } from './pages/book-details/book-details.component';
 import { AuthorDetailsComponent } from './pages/author-details/author-details.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     BookDetailsComponent,
-    AuthorDetailsComponent
+    AuthorDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
