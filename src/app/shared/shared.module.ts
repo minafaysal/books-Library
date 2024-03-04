6552504cbc3568f0e87f089baa4f05f254b53500
Mina Faysal <m.faysal@glamera.net>
2024-03-04 12:11:44 +0200

    update author loading ui & shared module routing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PagesRoutingModule } from '../pages/pages-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent, NotFoundComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule, PagesRoutingModule],
+  exports: [HeaderComponent, NotFoundComponent],
 })
 export class SharedModule {}
